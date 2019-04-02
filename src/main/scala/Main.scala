fffc98867df28ac6359945bd3314d1342bd183d6
Damien <damien_sim@hotmail.com>
2019-04-02 19:18:57 +0800

    updated main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,6 @@
-import java.io.PrintWriter
-
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
-
-import annotation.tailrec
-import scala.reflect.ClassTag
-
 import java.text.SimpleDateFormat
 
 case class DataPoint(driverID: String, orderID: String, minute: Int, longitude: Float, latitude: Float) extends Serializable
@@ -18,17 +11,19 @@ object Main extends Main {
   @transient lazy val sc: SparkContext = new SparkContext(conf)
 
   def main(args: Array[String]): Unit = {
-    val interestLongitude = 0f
-    val interestLatitude = 0f
+
+    val interestLongitude = 108.99680f
+    val interestLatitude = 34.25000f
     val start = 7*60 + 25
     val end = 7*60 + 26
+    val k = 1
     val lines = sc.textFile("data/didi_sample_data")
     val dataPoints = rawDataPoints(lines)
-    //dataPoints.saveAsTextFile("data/out/test.txt")
     val trajectories = getTrajectories(dataPoints)
     val filteredTrajectories = filterTrajectoriesByTime(trajectories, start, end)
-
-    filteredTrajectories.saveAsTextFile("data/out/test.txt")
+    val trajectoriesWithDistance = appendMinimumDistance(filteredTrajectories, interestLongitude, interestLatitude)
+    val result = sc.parallelize(trajectoriesWithDistance.take(k)).map(x => (x._1, x._3))
+    result.saveAsTextFile("data/out/result3.txt")
   }
 }
 
@@ -52,21 +47,34 @@ class Main extends Serializable {
   }
 
   /** Group the trajectories together */
-  def getTrajectories(dataPoints: RDD[DataPoint]): RDD[(String, Iterable[DataPoint])] = {
-    dataPoints.map(dp => (dp.orderID, dp)).groupByKey()
+  def getTrajectories(dataPoints: RDD[DataPoint]): RDD[(String, Iterable[(String, Int, Float, Float)])] = {
+    dataPoints.map(dp => (dp.orderID, dp.minute, dp.longitude, dp.latitude)).groupBy(_._1)
   }
 
   /** Filter the trajectories based on user input time */
-  def filterTrajectoriesByTime(dataPoints: RDD[(String, Iterable[DataPoint])], start: Int, end: Int):
-  RDD[(String, Iterable[DataPoint])] = {
+  def filterTrajectoriesByTime(dataPoints: RDD[(String, Iterable[(String, Int, Float, Float)])], start: Int, end: Int):
+  RDD[(String, Iterable[(Float, Float)])] = {
     dataPoints.filter { case (key, xs) =>
       xs.exists { xss =>
         if (start <= end) {
-          (xss.minute >= start) && (xss.minute <= end)
+          (xss._2 >= start) && (xss._2 <= end)
         } else {
-          (xss.minute >= start) && (xss.minute <= MAX_MINUTE) || (xss.minute >= 0) && (xss.minute <= end)
+          (xss._2 >= start) && (xss._2 <= MAX_MINUTE) || (xss._2 >= 0) && (xss._2 <= end)
         }
       }
-    }
+    }.map(x => (x._1, x._2.map(x => (x._3, x._4))))
+  }
+
+  /** Return trajectories with minimum distance to query point sorted by distance in ascending order **/
+  def appendMinimumDistance(trajectory: RDD[(String, Iterable[(Float, Float)])], queryLong: Float, queryLat: Float) :
+  RDD[(String, Double, Iterable[(Float, Float)])] = {
+    trajectory.map(x => (x._1, x._2.map(
+      x => euclideanDistance(x._1, x._2, queryLong, queryLat))
+      .reduce((x,y) => List(x,y).min), x._2)).sortBy(_._2)
+  }
+
+  /** Return the euclidean distance between two points */
+  def euclideanDistance(x1: Float, y1: Float, x2: Float, y2: Float): Double = {
+    Math.sqrt(Math.pow(x2 - x1,2) +  Math.pow(y2 - y1,2))
   }
 }
